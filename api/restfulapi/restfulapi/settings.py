70059c885133524277ba34ac2fd7ea6e3fafb9b9
Margo Tang <margo@aishk.com>
2021-04-20 13:35:47 +0800

    "add"

--- api/restfulapi/restfulapi/settings.py
+++ api/restfulapi/restfulapi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'bus',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'restfulapi.urls'
@@ -132,3 +134,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
--- sendCoordinate.py
+++ sendCoordinate.py
@@ -1,25 +1,33 @@
 import json
 import threading
 import redis
+import requests
 
-f = open('car.json',)
+f = open('station.json',)
 data = json.load(f)
 f.close()
 counter = 0
-host = localhost  # redis address
-port = port  # redis port
-pwd = pwd  # if there is auth needed
-redisPool = redis.ConnectionPool(host=host, port=port, password=pwd)
-client = redis.Redis(connection_pool=redisPool)
-
-
-def schedTask(counter, car):
-    print(data.get('car')[counter])
-    data_to_redis = json.dumps(data.get('car')[counter])
-    client.set('car', data_to_redis)  # setting key:value pair to redis
+# host = localhost  # redis address
+# port = port  # redis port
+# pwd = pwd  # if there is auth needed
+# redisPool = redis.ConnectionPool(host=host, port=port, password=pwd)
+# client = redis.Redis(connection_pool=redisPool)
+url = 'http://localhost:8000/api/bus/1/'
+headers = {
+    'Content-Type': 'application/json'
+}
+def schedTask(counter, bus):
+    print(data.get('bus')[counter])
+    data_to_redis = json.dumps(data.get('bus')[counter])
+    # client.set('car', data_to_redis)  # setting key:value pair to redis
+    resp = requests.put(url=url, headers=headers, data= data_to_redis)
     counter += 1
-    if counter < len(data.get('car')):
+    if counter < len(data.get('bus')):
         threading.Timer(5.0, schedTask, args=(counter, ), kwargs=data).start()
+    else:
+        counter = 0
+        threading.Timer(5.0, schedTask, args=(counter, ), kwargs=data).start()
+
 
 
 schedTask(counter, data)
